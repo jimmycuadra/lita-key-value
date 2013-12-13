9168572a64c57df004001b5c2fa7bd4758f2236d
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-12-13 15:39:34 -0800

    Add routes.

--- lib/lita/handlers/key_value.rb
+++ lib/lita/handlers/key_value.rb
@@ -3,6 +3,23 @@ require "lita"
 module Lita
   module Handlers
     class KeyValue < Handler
+      KEY_PATTERN = /[\w\._]+/
+
+      route(/^kv\s+set\s+(#{KEY_PATTERN.source})\s+(.+)/i, :set, command: true, help: {
+        "kv set KEY VALUE" => "Set KEY to VALUE."
+      })
+
+      route(/^kv\s+get\s+(#{KEY_PATTERN.source})/i, :get, command: true, help: {
+        "kv get KEY" => "Get the value of KEY."
+      })
+
+      route(/^kv\s+delete\s+(#{KEY_PATTERN.source})/i, :delete, command: true, help: {
+        "kv delete KEY" => "Delete KEY."
+      })
+
+      route(/^kv\s+list/i, :list, command: true, help: {
+        "kv list" => "List all keys."
+      })
     end
 
     Lita.register_handler(KeyValue)
--- spec/lita/handlers/key_value_spec.rb
+++ spec/lita/handlers/key_value_spec.rb
@@ -1,4 +1,8 @@
 require "spec_helper"
 
 describe Lita::Handlers::KeyValue, lita_handler: true do
+  it { routes_command("kv set foo bar").to(:set) }
+  it { routes_command("kv get foo").to(:get) }
+  it { routes_command("kv delete foo").to(:delete) }
+  it { routes_command("kv list").to(:list) }
 end
